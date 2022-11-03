898ee260384b4dd44fcc4c0bf2b270480951b03c
James-Codes-7 <110839973+James-Codes-7@users.noreply.github.com>
2022-11-03 15:12:22 +0530

    Add files via upload

---
+++ InterviewPanalSimpleApplication/model/Candidate.java
@@ -0,0 +1,32 @@
+package interviewpanal.model;
+
+public class Candidate {
+    private String candidateName;
+    private String candidateDOB;
+    private String candidateGender;
+    private String candidateQualification;
+    public String getCandidateName() {
+        return candidateName;
+    }
+    public void setCandidateName(String candidateName) {
+        this.candidateName = candidateName;
+    }
+    public String getCandidateDOB() {
+        return candidateDOB;
+    }
+    public void setCandidateDOB(String candidateDOB) {
+        this.candidateDOB = candidateDOB;
+    }
+    public String getCandidateGender() {
+        return candidateGender;
+    }
+    public void setCandidateGender(String candidateGender) {
+        this.candidateGender = candidateGender;
+    }
+    public String getCandidateQualification() {
+        return candidateQualification;
+    }
+    public void setCandidateQualification(String candidateQualification) {
+        this.candidateQualification = candidateQualification;
+    }
+}
---
+++ InterviewPanalSimpleApplication/model/CandidateDatabase.java
@@ -0,0 +1,34 @@
+package interviewpanal.model;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CandidateDatabase {
+
+    private static CandidateDatabase candidateDatabase;
+    private Queue<Candidate> candidateQueue;
+    private Queue<Candidate> candidateWaitingQueue;
+
+    public Queue<Candidate> getCandidateQueue() {
+        return candidateQueue;
+    }
+    public void setCandidateQueue(Candidate candidate) {
+        this.candidateQueue.offer(candidate);
+    }
+
+    public Queue<Candidate> getCandidateWaitingQueue() {
+        return candidateWaitingQueue;
+    }
+    public void setCandidateWaitingQueue(Candidate candidateWaitingQueue) {
+        this.candidateWaitingQueue.offer(candidateWaitingQueue);
+    }
+    public static CandidateDatabase getInstance()
+    {
+        if(candidateDatabase==null)candidateDatabase=new CandidateDatabase();
+        return candidateDatabase;
+    }
+    private CandidateDatabase()
+    {
+        candidateQueue=new LinkedList<>();
+    }
+}
